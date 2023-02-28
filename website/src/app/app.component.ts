4d8fcb3fb4dc0598a9c418dfceb4c7213f9e06ec
JupiterPi <jupiterpi@outlook.de>
2023-02-28 23:20:42 +0100

    better toc on mobile

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Observable} from "rxjs";
 import {PageStructureService} from "./page-structure.service";
 import {Page} from "./storage.service";
+import {Event as NavigationEvent, NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,29 @@ export class AppComponent {
   pageStructure: Observable<Page[][]>;
   activatedPage: Observable<string>;
 
-  constructor(private pageStructureService: PageStructureService) {
+  constructor(private pageStructureService: PageStructureService, private router: Router) {
     this.pageStructure = this.pageStructureService.getPageStructure();
     this.activatedPage = this.pageStructureService.getActivatedPage();
+
+    this.router.events.subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationEnd) {
+        this.expandToc = false;
+      }
+    });
+  }
+
+  expandToc = false;
+  toggleExpandToc() {
+    this.expandToc = !this.expandToc;
+  }
+
+  getActivatedPage(): Observable<Page> {
+    return new Observable(subscriber => {
+      this.activatedPage.subscribe(activatedPageId => {
+        this.pageStructure.subscribe(pageStructure => {
+          subscriber.next(pageStructure.flat().find(page => page.id == activatedPageId));
+        });
+      });
+    });
   }
 }
