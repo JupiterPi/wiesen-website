eef6b0a839c9fa6f65da27f6c52e52c2d05cfcc1
JupiterPi <jupiterpi@outlook.de>
2023-04-12 20:18:16 +0200

    overview page with existing pages

--- website/src/app/blog/blog-page/blog-page.component.ts
+++ website/src/app/blog/blog-page/blog-page.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {PageStructureService} from "../../page-structure.service";
+import {BlogPage} from "../../storage.service";
 
 @Component({
   selector: 'app-blog-page',
@@ -6,5 +8,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./blog-page.component.scss']
 })
 export class BlogPageComponent {
+  blogPages: BlogPage[] = [];
 
+  constructor(private pageStructureService: PageStructureService) {
+    this.pageStructureService.getBlockPages().subscribe(blogPages => {
+      this.blogPages = blogPages;
+    });
+  }
 }
