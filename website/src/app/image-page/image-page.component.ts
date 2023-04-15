08c16dcb6e8f8e0f9d9f8b0aa0b5c70e1a514a32
JupiterPi <jupiterpi@outlook.de>
2023-04-15 17:55:13 +0200

    nicer download button in image page

--- website/src/app/image-page/image-page.component.ts
+++ website/src/app/image-page/image-page.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Picture} from "../storage.service";
 import {PageStructureService} from "../page-structure.service";
 import {MarkdownParserService} from "../markdown-parser/markdown-parser.service";
 import {mdRoot} from '../markdown-parser/tree-types';
+import {ImageComponent} from "../markdown-parser/image/image.component";
+import {filter} from "rxjs";
+import {isNonNull} from "../util";
 
 @Component({
   selector: 'app-image-page',
@@ -14,6 +17,9 @@ export class ImagePageComponent {
   imageSrc?: string;
   picture?: Picture;
   descriptionNode?: mdRoot;
+
+  @ViewChild(ImageComponent) image?: ImageComponent;
+  downloadUrl = "wrong";
 
   constructor(private route: ActivatedRoute, private pageStructureService: PageStructureService, private markdownParser: MarkdownParserService) {
     this.route.params.subscribe(params => {
@@ -31,5 +37,14 @@ export class ImagePageComponent {
         }
       });
     });
+  }
+
+  getDownloadUrl() {
+    return this.image?.effectiveImageUrl().pipe(filter(isNonNull));
+  }
+
+  getFilename() {
+    const src = this.image?.picture?.src;
+    return src?.substring(src?.lastIndexOf("/") + 1);
   }
 }
